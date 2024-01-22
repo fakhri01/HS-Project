cb6891e7494b437f2e3b2d56d2e2b83c66b06de1
fakhri01 <habibzade.fakhri@gmail.com>
2024-01-23 03:16:25 +0400

    index.html updated and disqus added to tweets.html

---
+++ behavior.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const thumbnailElement = document.getElementById("smart_thumbnail");
+  let bool = false;
+
+  thumbnailElement.addEventListener("click", function () {
+    // write here
+    if (!bool) {
+      thumbnailElement.className = "small";
+        bool = true
+    } else {
+      thumbnailElement.className = "";
+      bool = false
+    }
+  });
+});
