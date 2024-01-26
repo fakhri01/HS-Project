9a49b01fdd8cfd6b3dcb1683a2179cc1c27d3ffc
fakhri01 <habibzade.fakhri@gmail.com>
2024-01-27 01:47:13 +0400

    functionality and responsive layout added  to the programs page

--- script-programs.js
+++ script-programs.js
@@ -9,5 +9,31 @@ document.addEventListener("DOMContentLoaded", function () {
       this.style.height = "250px";
     });
   });
+  const buttons = document.querySelectorAll(".btn");
+  const programs = document.querySelectorAll(".program-content");
   
+  programs[0].style.display = "flex"
+  
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", filterPosts.bind(this, buttons[i]));
+  }
+  
+
+  function filterPosts(item) {
+    changeActivePosition(item);
+    for (let i = 0; i < programs.length; i++) {
+      if (programs[i].classList.contains(item.attributes.id.value)) {
+        programs[i].style.display = "flex";
+      } else {
+        programs[i].style.display = "none";
+      }
+    }
+  }
+
+  function changeActivePosition(activeItem) {
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove("primary-btn");
+    }
+    activeItem.classList.add("primary-btn");
+  }
 });
